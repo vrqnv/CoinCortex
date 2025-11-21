8a7d4f8d7e6152fcd2aaadc3fec6bd1e63714427
Maksim Tikshaev <tikshaev.ycheba@gmail.com>
2025-11-21 23:43:49 +0400

    forms + design

--- CoinCortex/main/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ['author', 'content', 'created', 'likes_count']
-    list_filter = ['created']
-    search_fields = ['content', 'author__username']
--- CoinCortex/main/models.py
+++ CoinCortex/main/models.py
@@ -1,16 +1,33 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    wall_owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='wall_posts')
     content = models.TextField()
-    created = models.DateTimeField(default=timezone.now)
-    likes_count = models.IntegerField(default=0)
+    created = models.DateTimeField(auto_now_add=True)
+    wall_owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='wall_posts')
+    
+    def __str__(self):
+        return f'Post by {self.author} on {self.created}'
 
-    class Meta:
-        ordering = ['-created']
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    bio = models.TextField(max_length=500, blank=True)
 
     def __str__(self):
-        return f'Post by {self.author.username}'
+        return f'Profile of {self.user.username}'
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.get_or_create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    try:
+        instance.profile.save()
+    except Profile.DoesNotExist:
+        Profile.objects.create(user=instance)
